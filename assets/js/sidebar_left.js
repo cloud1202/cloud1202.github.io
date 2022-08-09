fc558ca6cccf2bedaa4acf88164e660546c4a891
cloud1202 <qorspdyeh@naver.com>
2022-08-09 18:42:35 +0900

    Js Updatae

---
+++ assets/js/sidebar_left.js
@@ -0,0 +1,62 @@
+window.addEventListener('resize', function(){
+    var icons =document.getElementsByTagName('svg');
+    for(let i = 0; i < icons.length;i++)
+    {
+        if(window.matchMedia('(min-width: 768px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 768px) and (max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-sm');
+        }
+        else if(window.matchMedia('(max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-2x');
+        }
+    }
+}, true);
+
+window.onbeforeunload = function(e){
+    var icons =document.getElementsByTagName('svg');
+    for(let i = 0; i < icons.length;i++)
+    {
+        if(window.matchMedia('(min-width: 768px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 768px) and (max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-sm');
+        }
+        else if(window.matchMedia('(max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-2x');
+        }
+    }
+}
+// la-width : 1024 , md-width : 768
