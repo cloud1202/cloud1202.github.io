ab6514408a9b825757c418374c13ed8f6e887e85
cloud1202 <qorspdyeh@naver.com>
2022-08-12 16:38:19 +0900

    Test and Layout Update
    Post Test
    Update : sidebar-left,
    sidebar-category,
    header

---
+++ assets/js/default.js
@@ -0,0 +1,67 @@
+// sidebar-left social icon size change
+// la-width : 1024 , md-width : 768
+window.onload = function(e){
+    var icons =document.getElementsByTagName('svg');
+    for(let i = 0; i < icons.length;i++)
+    {
+        if(window.matchMedia('(max-width: 768px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 768px) and (max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+    }
+    var body = document.body,
+        html = document.documentElement;
+
+    var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    document.getElementById("sidebar-left").style.height = height+"px";
+    
+}
+
+window.addEventListener('resize', function(){
+    var icons =document.getElementsByTagName('svg');
+    for(let i = 0; i < icons.length;i++)
+    {
+        if(window.matchMedia('(max-width: 768px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 768px) and (max-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+        }
+        else if(window.matchMedia('(min-width: 1441px)').matches)
+        {
+            icons[i].classList.remove('fa-2x');
+            icons[i].classList.remove('fa-sm');
+            icons[i].classList.remove('fa-xs');
+            icons[i].classList.remove('fa-lg');
+            icons[i].classList.add('fa-lg');
+        }
+    }
+}, true);
