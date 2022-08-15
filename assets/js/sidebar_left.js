649ba59ddcf0d2e4cb8dec1ea2cf06b687bae832
cloud1202 <qorspdyeh@naver.com>
2022-08-15 22:50:54 +0900

    js Update, favicon
    Add Jquery
    Add favicon

--- assets/js/sidebar_left.js
+++ assets/js/sidebar_left.js
@@ -1,14 +1,17 @@
-function OnClickDropdown(dropdown){
+function OnClickDropdown(dropdown, index){
     // state 0 = up, 1 = down
     const imoji_down_arrow = "&#x1F53D";
     const imoji_up_arrow = "&#x1F53C";
 
-    console.log(dropdown.getAttribute(dropdown.getAttribute('name') + " " + imoji_down_arrow));
-    if(true)
+  if (dropdown.getAttribute('state') == 1)
     {
+      dropdown.innerHTML = dropdown.getAttribute('name') + " " + imoji_down_arrow;
+    dropdown.setAttribute('state', '0');
     }
     else
     {
-        dropdown.innerHTML = dropdown.getAttribute('name') + " " + imoji_up_arrow;
-    }
+      dropdown.innerHTML = dropdown.getAttribute('name') + " " + imoji_up_arrow;
+      dropdown.setAttribute('state', '1');
+  }
+  $("#dropdown-item-" + index).slideToggle("fast");
 }
