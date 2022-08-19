ff30a79927f00e9eb82cb5a6b5820c5d121e5e9f
cloud1202 <qorspdyeh@naver.com>
2022-08-19 14:52:18 +0900

    Add Search
    Add search

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,7 +1,30 @@
 // sidebar-left social icon size change
 // la-width : 1024 , md-width : 768
 window.onload = function(e){
-    var icons =document.getElementsByTagName('svg');
+    setSearchBox();
+
+    setSocialIcon();
+
+    setSidebarHeight();
+}
+
+function setSearchBox()
+{
+    var searchText = localStorage.getItem('search_text');
+    
+    if(searchText === "" || searchText === null || searchText.trim().length === 0)
+    {
+        localStorage.removeItem('search_text');
+        return;
+    }
+    document.getElementById('search-text').value = searchText;
+
+    searchPost(searchText);
+}
+
+function setSocialIcon()
+{
+    var icons = document.getElementsByTagName('svg');
     for(let i = 0; i < icons.length;i++)
     {
         if(icons[i].parentElement.classList.contains('social-icon') == false)
@@ -33,55 +56,25 @@ window.onload = function(e){
             icons[i].classList.add('fa-lg');
         }
     }
+}
+
+function setSidebarHeight()
+{
     var body = document.body,
         html = document.documentElement;
 
     var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-    document.getElementById("sidebar-left").style.height = height+"px";
-    document.getElementById("sidebar-right").style.height = height+"px";
-    
+    document.getElementById("sidebar-left").style.height = height + "px";
+    document.getElementById("sidebar-right").style.height = height + "px";
 }
 
 window.onbeforeunload = function(e){
-    
-    var body = document.body,
-        html = document.documentElement;
+    setSearchBox();
 
-    var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-    document.getElementById("sidebar-left").style.height = height+"px";
-    document.getElementById("sidebar-right").style.height = height+"px";
+    setSidebarHeight();
 }
 
 window.addEventListener('resize', function(){
-    var icons =document.getElementsByTagName('svg');
-    for(let i = 0; i < icons.length;i++)
-    {
-        if(icons[i].parentElement.classList.contains('social-icon') == false)
-        {
-            continue;
-        }
-        if(window.matchMedia('(max-width: 768px)').matches)
-        {
-            icons[i].classList.remove('fa-2x');
-            icons[i].classList.remove('fa-sm');
-            icons[i].classList.remove('fa-xs');
-            icons[i].classList.remove('fa-lg');
-            icons[i].classList.add('fa-lg');
-        }
-        else if(window.matchMedia('(min-width: 768px) and (max-width: 1441px)').matches)
-        {
-            icons[i].classList.remove('fa-2x');
-            icons[i].classList.remove('fa-sm');
-            icons[i].classList.remove('fa-xs');
-            icons[i].classList.remove('fa-lg');
-        }
-        else if(window.matchMedia('(min-width: 1441px)').matches)
-        {
-            icons[i].classList.remove('fa-2x');
-            icons[i].classList.remove('fa-sm');
-            icons[i].classList.remove('fa-xs');
-            icons[i].classList.remove('fa-lg');
-            icons[i].classList.add('fa-lg');
-        }
-    }
+    setSocialIcon();
+
 }, true);
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,63 @@
+function EnterKeyPress(baseURL)
+{
+  if(window.event.keyCode == 13)
+  {
+    OnClickSearchButton(baseURL);
+  }
+}
+
 function OnClickSearchButton(baseURL)
 {
-  alert(baseURL+"\n"+document.location.href);
+  var searchText = document.getElementById('search-text').value;
+
+  if(searchText === "" || searchText === null || searchText.trim().length === 0)
+  {
+    alert("검색어를 입력해주세요.");
+    postReset();
+    document.getElementById('search-text').value = '';
+    localStorage.removeItem('search_text');
+    return;
+  }
+    
+  if(baseURL != document.location.href)
+  {
+    localStorage.setItem('search_text', searchText);
+    document.location.href = baseURL;
+  }
+
+  searchPost(searchText);
+}
+
+function searchPost(searchText)
+{
+  var data = document.getElementsByClassName("post-title");
+
+  var searchTexts = searchText.split(' ');
+
+  for(let i = 0; i < searchTexts.length; i++)
+  {
+    for(let j = 0; j < data.length; j++)
+    {
+      var regex = new RegExp(searchText[i], "i");
+  
+      if(regex.test(data[j].innerText) == false)
+      {
+        data[j].parentNode.style.display = 'none';
+      }
+      else
+      {
+        data[j].parentNode.style.display = '';
+      }
+    }
+  }
+}
+
+function postReset()
+{
+  var data = document.getElementsByClassName("post-entry");
+
+  for(let i = 0; i < data.length; i++)
+  {
+    data[i].style.display = '';
+  }
 }
