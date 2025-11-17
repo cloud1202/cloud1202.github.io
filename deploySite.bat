1c24bc25853d4badfdc4813f7fa985685dd319b0
cloud1202 <qorspdyeh@naver.com>
2025-11-17 14:31:36 +0900

    Update site 2025-11-17 14:31:36.79

--- deploySite.bat
+++ deploySite.bat
@@ -4,12 +4,14 @@ REM 2) GitHub Pages 배포용 deploy.bat
 REM =====================================================
 :: deploy.bat
 cd /d "%~dp0"
-bundle exec jekyll clean
-bundle exec jekyll build
 
 REM Git add, commit, push
 set COMMITMSG=Update site %date% %time%
 git add .
 git commit -m "%COMMITMSG%"
 git push origin main
+
+bundle exec jekyll clean
+bundle exec jekyll build
+
 pause
